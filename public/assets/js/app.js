96148509ca68d5a893ae172a4b905dc038995494
Kist221 <Kist221@gmail.com>
2017-12-09 16:15:16 -0700

    moved styling to css

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,7 +1,5 @@
 // Javascript for App Page
 
-
-
 // Initialize Firebase
  var config = {
    apiKey: "AIzaSyCVB25IfiYdoecRxag3WQlxtFkBAzqbcYs",
@@ -14,12 +12,18 @@
  
  firebase.initializeApp(config);
 
+
+
+
+
+
+
+
+// LOGIN AUTH 
 // Sign in anonymously function
 firebase.auth().signInAnonymously().catch(function(error) {
-  // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
-  // ...
   console.log(errorCode, errorMessage);
 });
 
@@ -28,60 +32,61 @@ firebase.auth().onAuthStateChanged(function(user) {
     // User is signed in.
     var isAnonymous = user.isAnonymous;
     var uid = user.uid;
-    // ...
     console.log(isAnonymous, uid);
   } else {
     // User is signed out.
-    // ...
   }
-  // ...
 });
 
-    // Create an initial choreCount variable
-    var choreCount = 0;
-
-    //  On Click event associated with the add-chore function
-    $("#add-chore").on("click", function(event) {
-      event.preventDefault();
-
-    // Get the to-do "value" from the textbox and store it a variable
-    var newChore = $("#chore").val().trim();
 
 
-    // 
-    var toDoChore = $("<p>");
-
-      toDoChore.attr("id", "item-" + choreCount);
-      toDoChore.append(" " + newChore);
 
 
-    // 
-    var choreClose = $("<button>");
+// Create an initial choreCount variable
+var choreCount = 0;
 
-      choreClose.attr("data-chore", choreCount);
-      choreClose.addClass("checkbox");
-      choreClose.append("&check;");
+//  On Click event associated with the add-chore function
+$("#add-chore").on("click", function(event) {
+  event.preventDefault();
 
-    // Append the button to the to do item
-      toDoChore = toDoChore.prepend(choreClose);
+// Get the to-do "value" from the textbox and store it a variable
+var newChore = $("#chore").val().trim();
 
-    // Add the button and chore item to the chore-list div
-      $("#chore-list").append(toDoChore);
 
-    // Clear the textbox when done
-      $("#chore").val("");
+// 
+var toDoChore = $("<p>");
 
-    // Add to the toDoCount
-      choreCount++;
-    });
+  toDoChore.attr("id", "item-" + choreCount);
+  toDoChore.append(" " + newChore);
 
-       $(document.body).on("click", ".checkbox", function() {
 
-      // Get the number of the button from its data attribute and hold in a variable called  choreNumber.
-      var choreNumber = $(this).attr("data-chore");
+// 
+var choreClose = $("<button>");
 
-      // Select and Remove the specific <p> element that previously held the to do item number.
-      $("#item-" + choreNumber).remove();
-    });
+  choreClose.attr("data-chore", choreCount);
+  choreClose.addClass("checkbox");
+  choreClose.append("&check;");
+
+// Append the button to the to do item
+  toDoChore = toDoChore.prepend(choreClose);
+
+// Add the button and chore item to the chore-list div
+  $("#chore-list").append(toDoChore);
+
+// Clear the textbox when done
+  $("#chore").val("");
+
+// Add to the toDoCount
+  choreCount++;
+});
+
+   $(document.body).on("click", ".checkbox", function() {
+
+  // Get the number of the button from its data attribute and hold in a variable called  choreNumber.
+  var choreNumber = $(this).attr("data-chore");
+
+  // Select and Remove the specific <p> element that previously held the to do item number.
+  $("#item-" + choreNumber).remove();
+});
 
 
