cc38d734a60e811fb9c5672331b40cd95d84b05f
Kist221 <Kist221@gmail.com>
2017-12-16 13:33:59 -0700

    added sign up and login for email

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -17,19 +17,25 @@ var db = firebase.database();
 var uid = null;
 
 // LOGIN AUTH CODE
-// add click events for login/logout
-$("#btnLogin").on("click", function(){
+// click event for LOGIN
+$("#loginForm").on("click", "#btnLogin", function(){
+  event.preventDefault();
   // Sign in anonymously function
-  firebase.auth().signInAnonymously();
-  $("#myApplication").removeClass("hidden");
+  // firebase.auth().signInAnonymously();
+  // run SIGN IN function
+  handleSignIn();
 });
-
+// click event for SIGN UP
+$("#loginForm").on("click", "#btnSignup", function(){
+  event.preventDefault();
+  // run SIGN UP function
+  handleSignUp();
+});
 // click event for LOGOUT
 $("#btnLogout").on("click", function(){
   // logout user
   firebase.auth().signOut();
   uid = null;
-  $("#myApplication").addClass("hidden");
 });
 
 // calls function when user value is changed
@@ -51,8 +57,77 @@ firebase.auth().onAuthStateChanged(function(user) {
 });
 
 
-// DATABASE MANIPULATION FOR FIREBASE
+// Function to SIGN IN
+function handleSignIn() {
+  if (firebase.auth().currentUser) {
+    // [START signout]
+    firebase.auth().signOut();
+    // [END signout]
+  } else {
+    var email = document.getElementById('emailInput').value;
+    var password = document.getElementById('passInput').value;
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+    // Sign in with email and pass.
+    // [START authwithemail]
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+      document.getElementById('btnLogin').disabled = false;
+      // [END_EXCLUDE]
+    });
+    // [END authwithemail]
+  }
+  document.getElementById('btnLogin').disabled = true;
+};
 
+
+// Function to SIGN UP
+function handleSignUp() {
+  var email = document.getElementById('emailInput').value;
+  var password = document.getElementById('passInput').value;
+  if (email.length < 4) {
+    alert('Please enter an email address.');
+    return;
+  }
+  if (password.length < 4) {
+    alert('Please enter a password.');
+    return;
+  }
+  // Sign in with email and pass.
+  // [START createwithemail]
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // [START_EXCLUDE]
+    if (errorCode == 'auth/weak-password') {
+      alert('The password is too weak.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+    // [END_EXCLUDE]
+  });
+  // [END createwithemail]
+}
+
+
+// DATABASE MANIPULATION FOR FIREBASE
 
 // Create an initial timeNow variable
 var timeNow = Date.now();
